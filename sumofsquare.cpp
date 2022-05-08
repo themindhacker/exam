9a29dcf501c5428ff66d51b71d92700e26f3f739
themindhacker <73572307+themindhacker@users.noreply.github.com>
2022-05-08 19:56:11 +0530

    Update sumofsquare.cpp

--- sumofsquare.cpp
+++ sumofsquare.cpp
@@ -7,6 +7,6 @@ cout<<"Enter the limit";
 cin>>N;
 for (i=1; i<=N; i++)
 	sum = sum + i*i;
-cout<<"\n Sum of squares of first "<<N<<"natural numbers="<<sum;
+cout<<"\n Sum of squares of first "<<N<<" natural numbers="<<sum;
 return 0;
  }
